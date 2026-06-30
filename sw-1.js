a07a583f839b39bc2724a6f8772c580b678ff9f3
winwin-sdi <myungchulkim@kakao.com>
2026-06-30 15:15:40 +0900

    Add files via upload

---
+++ sw-1.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
